f15236b562c45b8e5ee1bc6a7d1770027e87b8c5
Ruiya Lin <ruiyalin0623@gmail.com>
2024-11-01 12:26:19 +0000

    finalize

--- WK4_Gender inequality index.R
+++ WK4_Gender inequality index.R
@@ -1,3 +1,9 @@
+#date frame
+#worldcities: World_Countries_Generalized.geojson
+#worldcities_all: Composite_indices_1990_2022.csv
+#worldcities_gii: iso3 and gii from 1990 to 2022
+#worldcities_gii_diff: iso2 and gii difference 
+
 #Import all libraries
 library(tmap)
 library(tmaptools)
@@ -9,7 +15,7 @@ library(janitor) #install_packages('') if needed
 library(ggplot2)
 
 # read geojson file, quick map 
-worldcities <- st_read("World_Countries_Generalized.geojson")
+worldcities <- st_read("Data/World_Countries_Generalized.geojson")
 qtm(worldcities)
 
 # open csv. file
@@ -22,7 +28,7 @@ worldcities_all <-read.csv(here::here("Data/Composite_indices_1990_2022.csv"),
 worldcities_gii <- worldcities_all %>%
   select(iso3, country, matches("^gii_19[9][0-9]$|^gii_20[0-2][0-9]$"))
 
-#create a new column for iso2
+#create a new column for iso2; new function countrycode()
 worldcities_gii$iso2 <- countrycode(worldcities_gii$iso3, origin = 'iso3c', destination = 'iso2c')
 head(worldcities_gii)#check it
 
@@ -39,19 +45,17 @@ worldcities_gii_diff <- worldcities_gii %>%
   select(iso2, diff)
 head(worldcities_gii_diff)
 
-# Merge the map and csv df
-gii_diff <- clean_names(worldcities_gii_diff)
-worldcities_map <- worldcities %>%
+# Merge the map and csv df; left join
+worldcities_gii_diff_clean <- clean_names(worldcities_gii_diff)
+Gii_Map <- worldcities %>%
   clean_names() %>%
-  merge(.,
-        gii_diff,
-        by.x="iso", 
-        by.y="iso2",
-        no.dups = TRUE) %>%
-  distinct(., iso, 
-           .keep_all = TRUE)
+  left_join(gii_diff, by = c("iso" = "iso2")) %>%
+  distinct(iso, .keep_all = TRUE)
 
-#left join
-gii_diff <- worldcities_gii
-
-#
+#Make a map
+ggplot(Gii_Map) +
+  geom_sf(aes(fill = diff)) + 
+  scale_fill_gradient2(low = "blue", mid = "lightblue", high = "red", 
+                       midpoint = 0, na.value = "grey") 
+theme_minimal() +
+  ggtitle("GII difference between 1990 and 2022")
